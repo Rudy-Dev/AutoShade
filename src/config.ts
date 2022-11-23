9566091875cbf056d482eae958be792c37235e52
Wiktor Rudnicki <mazepianko@gmail.com>
2022-11-23 18:09:26 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import { $env } from "rbxts-transform-env";
 export const Version = $env.string("VERSION", "1.0.0");
 export const Dev = $env.boolean("DEV", false);
 export const DefaultValue = $env.number("DEFAULT_VALUE", 0.05);
-export const Icon = $env.string("ICON", "rbxassetid://11621392294");
+export const Icon = $env.string("ICON", "rbxassetid://11643440439");
 export const Name = $env.string("NAME", "AutoShade");
