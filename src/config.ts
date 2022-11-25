9a4694515cc57add2a76e675c0afbf14fe7d389c
Wiktor Rudnicki <mazepianko@gmail.com>
2022-11-25 21:23:18 +0100

    Fixed runtime error. 1.0.1 Fixed start size of the widget. Changed logo. Updated eslint settings.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { $env } from "rbxts-transform-env";
 
-export const Version = $env.string("VERSION", "1.0.0") as string;
-export const Dev = $env.boolean("DEV", false) as boolean;
-export const DefaultValue = $env.number("DEFAULT_VALUE", 0.05) as number;
-export const Icon = $env.string("ICON", "rbxassetid://11621392294") as string;
-export const Name = $env.string("NAME", "AutoShade") as string;
+export const Version = $env.string("VERSION", "1.0.0");
+export const Dev = $env.boolean("DEV", false);
+export const DefaultValue = $env.number("DEFAULT_VALUE", 0.05);
+export const Icon = $env.string("ICON", "rbxassetid://11621392294");
+export const Name = $env.string("NAME", "AutoShade");
--- src/index.server.ts
+++ src/index.server.ts
@@ -2,31 +2,32 @@ import Roact from "@rbxts/roact";
 import app from "app";
 import { Icon, Name } from "config";
 import { $env } from "rbxts-transform-env";
+if (game.GetService("RunService").IsEdit()) {
+	const toolbar = plugin.CreateToolbar(Name);
+	const button = toolbar.CreateButton(Name, "Toggle AutoShade", Icon);
+	const widget = plugin.CreateDockWidgetPluginGui(
+		Name,
+		new DockWidgetPluginGuiInfo(Enum.InitialDockState.Float, false, false, 150, 250, 150, 250),
+	);
+	const IS_DEV = $env.boolean("DEV");
 
-const toolbar = plugin.CreateToolbar(Name);
-const button = toolbar.CreateButton(Name, "Toggle AutoShade", Icon);
-const widget = plugin.CreateDockWidgetPluginGui(
-	Name,
-	new DockWidgetPluginGuiInfo(Enum.InitialDockState.Float, false, false, 150, 150, 150, 250),
-);
-const IS_DEV = $env.boolean("DEV");
+	widget.Name = Name;
+	widget.Title = Name;
 
-widget.Name = Name;
-widget.Title = Name;
-
-let enabled = false;
-button.Click.Connect(() => {
-	enabled = !enabled;
-	widget.Enabled = enabled;
-});
-
-if (!IS_DEV) {
-	widget.BindToClose(() => {
-		enabled = false;
-		//hacky way to get the button to update
-		plugin.Activate(true);
-		plugin.Activate(false);
+	let enabled = false;
+	button.Click.Connect(() => {
+		enabled = !enabled;
+		widget.Enabled = enabled;
 	});
-}
 
-Roact.mount(Roact.createElement(app, { Plugin: plugin }), widget);
+	if (!IS_DEV) {
+		widget.BindToClose(() => {
+			enabled = false;
+			//hacky way to get the button to update
+			plugin.Activate(true);
+			plugin.Activate(false);
+		});
+	}
+
+	Roact.mount(Roact.createElement(app, { Plugin: plugin }), widget);
+}
